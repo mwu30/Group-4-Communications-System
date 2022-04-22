b57ebb0576e3a295fa1ba9c8ac12a73622c1d829
mwu30 <71101983+mwu30@users.noreply.github.com>
2022-04-21 18:23:05 -0700

    Added rest of member variables+methods

--- src/Message.java
+++ src/Message.java
@@ -1,44 +1,94 @@
 import java.io.Serializable;
+import java.util.Date;
 
 public class Message implements Serializable {
     protected String type;
-    protected String status;
     protected String text;
+    protected String status;
+    protected User sender;
+    protected Date timeStamp; 
+    protected String[] roomList;
 
-    public Message(){
+    public Message() {    	
         this.type = "Undefined";
         this.status = "Undefined";
         this.text = "Undefined";
+    	Date currentDate = new Date();
+    	this.timeStamp = currentDate;
+    }
+    
+        public Message(String status) {
+    	setStatus(status);
+    	Date currentDate = new Date();
+    	this.timeStamp = currentDate;
     }
 
-    public Message(String type, String status, String text){
+    public Message(String type, String text) {
+    	setType(type);
+    	setText(text);
+    	Date currentDate = new Date();
+    	this.timeStamp = currentDate;
+    }
+    
+    // Possible constructor for sending messages to a chatroom
+    public Message(String type, String text, User sender, String[] roomList) {
+    	setType(type);
+    	setText(text);
+    	setSender(sender);
+        setRoomList(roomList);
+    	Date currentDate = new Date();
+    	this.timeStamp = currentDate;
+    }
+
+    public Message(String type, String status, String text) {
         setType(type);
         setStatus(status);
         setText(text);
+    	Date currentDate = new Date();
+    	this.timeStamp = currentDate;
     }
 
-    private void setType(String type){
+    private void setType(String type) {
     	this.type = type;
     }
 
-    public void setStatus(String status){
+    public void setStatus(String status) {
     	this.status = status;
     }
 
-    public void setText(String text){
+    public void setText(String text) {
     	this.text = text;
     }
+    
+    public void setSender(User sender) {
+    	this.sender = sender;
+    }
+    
+    public void setRoomList(String[] roomList) {
+    	this.roomList = roomList;
+    }
 
-    public String getType(){
+    public String getType() {
     	return type;
     }
 
-    public String getStatus(){
+    public String getStatus() {
     	return status;
     }
 
-    public String getText(){
+    public String getText() {
     	return text;
     }
-
+    
+    public User getSender() {
+    	return sender;
+    }
+    
+    public Date getTimeStamp() {
+    	return timeStamp;
+    }
+    
+    public String[] getRoomList() {
+    	return roomList;
+    }
 }
